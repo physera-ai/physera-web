f138265fd765363f79331e14ee4b2d648adfa60e
Shashwat Dubey <shashwat5590@gmail.com>
2026-05-03 21:22:03 +0530

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { funnelDisplay, headingFont, mono, sans, serifFont } from "./fonts";
 import "./globals.css";
+
+const GA_MEASUREMENT_ID = "G-XHRND0W3WV";
 
 export const metadata: Metadata = {
   title: "Physera | Rethinking Applied Intelligence",
@@ -31,6 +34,19 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${funnelDisplay.variable} ${headingFont.variable} ${sans.variable} ${mono.variable} ${serifFont.variable} h-full`}
     >
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <body className="min-h-full flex flex-col relative">
         {children}
       </body>
